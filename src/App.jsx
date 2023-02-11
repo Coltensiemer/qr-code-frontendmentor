284dfa67073004511ac360a9e54e597d06c43068
Colten Siemergit <colten50@hotmail.com>
2023-02-11 18:28:27 -0500

    updated for accessibiltiy

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from "/src/components/Footer"
 export default function App() { 
     return ( 
       
-        <div className="m-0">
+        <div className="m-0 bg-blue-100">
         <Main />
         <Footer />
         </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 export default function Footer() { 
     return (
-        <div>
-  <p className="text-base font-outfit text-center text-slate-300">Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        <Fragment>
+  <p className="text-base font-outfit text-center text-slate-900">Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
   Coded by <a href="#">Colten Richard Siemer</a>. </p> 
-</div>
+</Fragment>
     )
 }
